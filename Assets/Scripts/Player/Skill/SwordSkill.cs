3a86a516ff477f8f49779592e5b999fe7f64783b
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-08-04 02:02:08 +0900

    캐릭터 기본공격(콤보)

--- Assets/Scripts/Player/Skill/SwordSkill.cs
+++ Assets/Scripts/Player/Skill/SwordSkill.cs
@@ -11,30 +11,84 @@ public interface ISkill
 
 public class SwordSkill : MonoBehaviour, ISkill
 {
+    [SerializeField] float Speed;
+    [SerializeField] float AttackTimeCheck = 0; //기본공격 시간 측정용
+    [SerializeField] float AttackTime = 0; //기본공격이 다음공격으로 갈수 있는 시간(-0.2 ~ 0.2)
+    int AttackNum = 0;
+    [SerializeField] bool isAttack = false;
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)&& !isAttack)
+        {
+            Attack();
+        }
+    }
+
+
     public void Attack()
     {
+        isAttack = true;
+        AttackTimeCheck = 0;
+        StartCoroutine(AttackCombo());
+    }
 
+    IEnumerator AttackCombo()
+    {
+        PlayAnimation(AttackNum++);
+        yield return null;
+
+        while (AttackTimeCheck <= 1.3f || AttackNum<3)
+        {
+            if (AttackTimeCheck > 1.3f)
+                break;
+            AttackTimeCheck += Time.deltaTime * Speed;
+           if(Input.GetMouseButtonDown(0))
+            {
+                if (AttackTimeCheck >= AttackTime- 0.2f && AttackTimeCheck <= AttackTime+0.2f)
+                {
+                    if (AttackNum > 2)
+                    {
+                        AttackReset();
+                        yield break;
+                    }
+
+                    PlayAnimation(AttackNum++);
+                    AttackTimeCheck = 0;
+                }
+            }
+            
+            yield return null;
+        }
+
+        AttackReset();
+    }
+
+    private void AttackReset()
+    {
+        AttackNum = 0;
+        AttackTimeCheck = 0;
+        isAttack = false;
     }
 
     public void DashAtaack()
     {
-        throw new System.NotImplementedException();
     }
 
     public void Skill()
     {
-        throw new System.NotImplementedException();
     }
 
-    // Start is called before the first frame update
-    void Start()
+    void PlayAnimation(int ani)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        animator.SetFloat("Attack_Count", ani);
+        animator.SetTrigger("Attack");
     }
 }
